1070432fd24edf0c31137e2897de663501b2b597
mervezgin <mervezgin@gmail.com>
2024-04-17 10:56:34 +0300

    clean up code and added variables

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     float horizontalInput;
     float playerSpeed = 1.0f;
+    float leftBoundary = -20.0f;
+    float rightBoundary = 20.0f;
+    float xRange = 20.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +21,14 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * playerSpeed);
+
+        if (transform.position.x > rightBoundary)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < leftBoundary)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
     }
 }
